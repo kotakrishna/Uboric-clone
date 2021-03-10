504a0274c16cb4de9aca190cc77e72a57df92ce2
k.saikrishna <k.saikrishna78@gmail.com>
2021-03-10 14:45:05 +0530

    the data of the pages compelete

---
+++ src/js/grocery/data.js
@@ -0,0 +1,93 @@
+var data = {
+    0: {
+    //   Company_name:"Vasant",
+      tittle: "CHOCOLATE PEANUT BUTTER CRUNCHY (1 KG)",
+      price: 525.00,
+      description:"Peanut butter is served as a spread on bread, toast, or crackers, and used to make sandwiches (notably the peanut butter and jelly sandwich). It is also used in a number of breakfast dishes and desserts, such as peanut-flavored granola, smoothies, crepes, cookies, brownies, or croissants. It is similar to other nut butters such as cashew butter and almond butter.",
+      src:
+        "https://www.uboric.com/wp-content/uploads/2021/03/ra.png",
+    },
+    1: {
+    //   Company_name:"Vasant",
+      tittle: "Creamy Peanut Butter",
+      price: 499.00,
+      description:"Peanut butter is served as a spread on bread, toast, or crackers, and used to make sandwiches (notably the peanut butter and jelly sandwich). It is also used in a number of breakfast dishes and desserts, such as peanut-flavored granola, smoothies, crepes, cookies, brownies, or croissants. It is similar to other nut butters such as cashew butter and almond butter.",
+      src:
+        "https://www.uboric.com/wp-content/uploads/2021/03/vivek.png",
+        quantity:"1000 gm"
+    },
+    2: {
+    //   Company_name:"VASANT",
+      tittle: "HONEY CREAMY (1 KG)",
+      price: 545.00,
+      description:"Peanut butter is served as a spread on bread, toast, or crackers, and used to make sandwiches (notably the peanut butter and jelly sandwich). It is also used in a number of breakfast dishes and desserts, such as peanut-flavored granola, smoothies, crepes, cookies, brownies, or croissants. It is similar to other nut butters such as cashew butter and almond butter.",
+      src:
+        "https://www.uboric.com/wp-content/uploads/2021/03/rrr-1.png",
+        quantity:"1000 gm"
+    },
+    3: {
+    //   Company_name:"Vasant",
+      tittle: "Alpino Classic Peanut Butter Smooth 400 G | Made with Roasted Peanuts | 25% Protein | Non GMO | Gluten Free | Vegan",
+      price: 179.00,
+      description:"Oil Separation Is Natural Process, Stir Well Before Use Together With The Confidence Of Knowing Your Peanut Contains No Stabilizers Or Fillers.",
+      src:
+        "https://www.uboric.com/wp-content/uploads/2021/03/A-1.png",
+        quantity:"400 gm"
+    },
+    4: {
+    //   Company_name:"VASANT",
+      tittle: "Alpino Chocolate Peanut Butter Smooth 1 KG | Made with Roasted Peanuts, Cocoa Powder & Choco Chips | 20% Protein | Non GMO | Gluten Free | Vegan",
+      price: 449.00,
+      description:"Oil Separation Is Natural Process, Stir Well Before Use Together With The Confidence Of Knowing Your Peanut Butter Contains No Stabilizers Or Fillers.",
+      src:
+        "https://www.uboric.com/wp-content/uploads/2021/02/71xEnkqrBfL._SL1500_.jpg",
+        quantity:"1000 gm"
+    },
+    5: {
+        // Company_name:"VASANT",
+        tittle: "Alpino Super Muesli Fruit & Nuts 400 G | Whole Oats & Whole Grain | Finest Nuts & Raisins | Real Freeze Dried Fruits | No Sugar Infused Fruits",
+        price: 289.00,
+        description:"While we work to ensure that product information is correct, on occasion manufacturers may alter their ingredient lists. Actual product packaging and materials may contain more and/or different information than that shown on our web site.",
+        src:
+          "https://www.uboric.com/wp-content/uploads/2021/02/81I8P7aIiSL._SL1500_.jpg",
+          quantity:"1000 gm"
+      },
+    6: {
+    //   Company_name:"VASANT",
+      tittle: "Alpino Super Muesli Nut Delight 400 G | Whole Oats & Whole Grain | Finest Nuts & Raisins | Real Freeze Dried Fruits | No Sugar Infused Fruits",
+      price: 289.00,
+      description:" Actual product packaging and materials may contain more and/or different information than that shown on our web site. We recommend that you do not solely rely on the information presented and that you always read labels, warnings, and directions before using or consuming a product.",
+      src:
+        "https://www.uboric.com/wp-content/uploads/2021/02/81c4nPv0DQL._SL1500_.jpg",
+    quantity:"400 gm"
+    },
+    7: {
+        //Company_name:"Vasant",
+        tittle: "CHOCOLATE PEANUT BUTTER CRUNCHY (1 KG)",
+        price: 525.00,
+        description:"Peanut butter is served as a spread on bread, toast, or crackers, and used to make sandwiches (notably the peanut butter and jelly sandwich). It is also used in a number of breakfast dishes and desserts, such as peanut-flavored granola, smoothies, crepes, cookies, brownies, or croissants. It is similar to other nut butters such as cashew butter and almond butter.",
+        src:
+          "https://www.uboric.com/wp-content/uploads/2021/03/ra.png",
+      },
+    8: {
+        // Company_name:"Vasant",
+      tittle: "Creamy Peanut Butter",
+      price: 499.00,
+      description:"Peanut butter is served as a spread on bread, toast, or crackers, and used to make sandwiches (notably the peanut butter and jelly sandwich). It is also used in a number of breakfast dishes and desserts, such as peanut-flavored granola, smoothies, crepes, cookies, brownies, or croissants. It is similar to other nut butters such as cashew butter and almond butter.",
+      src:
+        "https://www.uboric.com/wp-content/uploads/2021/03/vivek.png",
+        quantity:"1000 gm"
+      },
+    9: {
+        //   Company_name:"VASANT",
+      tittle: "Alpino Chocolate Peanut Butter Smooth 1 KG | Made with Roasted Peanuts, Cocoa Powder & Choco Chips | 20% Protein | Non GMO | Gluten Free | Vegan",
+      price: 449.00,
+      description:"Oil Separation Is Natural Process, Stir Well Before Use Together With The Confidence Of Knowing Your Peanut Butter Contains No Stabilizers Or Fillers.",
+      src:
+        "https://www.uboric.com/wp-content/uploads/2021/02/71xEnkqrBfL._SL1500_.jpg",
+        quantity:"1000 gm"
+      }
+  };
+  
+  export default data;
+  
