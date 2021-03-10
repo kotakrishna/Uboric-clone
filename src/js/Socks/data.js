4b55b0b2c8b40c2499d7af3e8c637c611a7b1797
ashish8796 <ashishsaini.ak@gmail.com>
2021-03-10 14:23:54 +0530

    socks html and css completed @Ashish

--- src/js/Socks/data.js
+++ src/js/Socks/data.js
@@ -1,107 +1,133 @@
-var data={
-    0:{
-        Company_name:"Luxury Terry",
-        title:"Solid Ankle Socks BLACK PACK OF 3",
-        price:"199.00",
-        description:"Men’s Luxury Terry Cushioning and Spandex Solid Ankle Socks BLACK PACK OF 3 (FREE SIZE)",
-        src:"https://www.uboric.com/wp-content/uploads/2020/12/WCTA03-059_1024x1024.png"
-    },
-    1:{
-        Company_name:"Luxury Terry",
-        title:"ASHIKAGA EDITION SOCKS",
-        price:"299.00",
-        description:"Men’s Luxury Terry Cushioning and Spandex Solid Ankle Socks",
-        src:"https://www.uboric.com/wp-content/uploads/2020/11/socksoho-comfortable-fun-men-socks-stripes-ashikaga-edition-13861452611635_600x.png"
-    },
-    2:{
-        Company_name:"Luxury Terry",
-        title:"BLUEBERRY EDITION SOCKS",
-        price:"299.00",
-        description:"Men’s Luxury Terry Cushioning and Spandex BLUEBERRY EDITION SOCKS",
-        src:"https://www.uboric.com/wp-content/uploads/2020/11/Blueberry_2_600x.png"
-    },
-    3:{
-        Company_name:"CLASSIC BLACK",
-        title:"THE GENTLEMAN EDITION SOCKS",
-        price:"399.00",
-        description:"Men’s Luxury Terry Cushioning and Spandex LONDON EDITION SOCKS",
-        src:"https://www.uboric.com/wp-content/uploads/2020/11/Gentleman_600x.png"
-    },
-    4:{
-        Company_name:"CLASSIC BLACK",
-        title:"CLASSIC BLACK EDITION SOCKS",
-        price:"349.00",
-        description:"Men’s Luxury Terry Cushioning and Spandex CLASSIC BLACK EDITION SOCKS",
-        src:"https://www.uboric.com/wp-content/uploads/2020/11/socksoho-comfortable-men-socks-black-classic-black-edition-13861452644403_600x.png"
-    },
-    5:{
-        Company_name:"CLASSIC BLACK",
-        title:"HAPPY SOCKS GIFT BOX FOR MEN",
-        price:"1,599.00",
-        description:"Men’s Luxury Terry Cushioning and Spandex CLASSIC BLACK EDITION SOCKS",
-        src:"https://www.uboric.com/wp-content/uploads/2020/11/POWERBOX_c4c4b190-aba2-4629-8bfe-8e7d0507d99b_600x.png"
-    },
-    6:{
-        Company_name:"CLASSIC BLACK",
-        title:"LONDON EDITION SOCKS",
-        price:"349.00",
-        description:"Men’s Luxury Terry Cushioning and Spandex LONDON EDITION SOCKS",
-        src:"https://www.uboric.com/wp-content/uploads/2020/11/socksoho-cute-quirky-men-socks-london-edition-13862745604147_600x.png"
-    },
-    7:{
-        Company_name:"CLASSIC BLACK",
-        title:"THE GENTLEMAN EDITION SOCKS",
-        price:"399.00",
-        description:"Men’s Luxury Terry Cushioning and Spandex LONDON EDITION SOCKS",
-        src:"https://www.uboric.com/wp-content/uploads/2020/11/Gentleman_600x.png"
-    },
-    8:{
-        Company_name:"Luxury Terry",
-        title:"ASHIKAGA EDITION SOCKS",
-        price:"299.00",
-        description:"Men’s Luxury Terry Cushioning and Spandex Solid Ankle Socks",
-        src:"https://www.uboric.com/wp-content/uploads/2020/11/socksoho-comfortable-fun-men-socks-stripes-ashikaga-edition-13861452611635_600x.png"
-    },
-    9:{
-        Company_name:"CLASSIC BLACK",
-        title:"THE GENTLEMAN EDITION SOCKS",
-        price:"399.00",
-        description:"Men’s Luxury Terry Cushioning and Spandex LONDON EDITION SOCKS",
-        src:"https://www.uboric.com/wp-content/uploads/2020/11/Gentleman_600x.png"
-    },
-    10:{
-        Company_name:"CLASSIC BLACK",
-        title:"LONDON EDITION SOCKS",
-        price:"349.00",
-        description:"Men’s Luxury Terry Cushioning and Spandex LONDON EDITION SOCKS",
-        src:"https://www.uboric.com/wp-content/uploads/2020/11/socksoho-cute-quirky-men-socks-london-edition-13862745604147_600x.png"
-    },
-    11:{
-        Company_name:"CLASSIC BLACK",
-        title:"HAPPY SOCKS GIFT BOX FOR MEN",
-        price:"1,599.00",
-        description:"Men’s Luxury Terry Cushioning and Spandex CLASSIC BLACK EDITION SOCKS",
-        src:"https://www.uboric.com/wp-content/uploads/2020/11/POWERBOX_c4c4b190-aba2-4629-8bfe-8e7d0507d99b_600x.png"
-    },
-    12:{
-        Company_name:"CLASSIC BLACK",
-        title:"CLASSIC BLACK EDITION SOCKS",
-        price:"349.00",
-        description:"Men’s Luxury Terry Cushioning and Spandex CLASSIC BLACK EDITION SOCKS",
-        src:"https://www.uboric.com/wp-content/uploads/2020/11/socksoho-comfortable-men-socks-black-classic-black-edition-13861452644403_600x.png"
-    },
-    13:{
-        Company_name:"Luxury Terry",
-        title:"BLUEBERRY EDITION SOCKS",
-        price:"299.00",
-        description:"Men’s Luxury Terry Cushioning and Spandex BLUEBERRY EDITION SOCKS",
-        src:"https://www.uboric.com/wp-content/uploads/2020/11/Blueberry_2_600x.png"
-    },
-    14:{
-        Company_name:"Luxury Terry",
-        title:"ASHIKAGA EDITION SOCKS",
-        price:"299.00",
-        description:"Men’s Luxury Terry Cushioning and Spandex Solid Ankle Socks",
-        src:"https://www.uboric.com/wp-content/uploads/2020/11/socksoho-comfortable-fun-men-socks-stripes-ashikaga-edition-13861452611635_600x.png"
-    }
-}
+var data = {
+  0: {
+    company_name: "Luxury Terry",
+    tittle: "Solid Ankle Socks BLACK PACK OF 3",
+    price: "199.00",
+    description:
+      "Men’s Luxury Terry Cushioning and Spandex Solid Ankle Socks BLACK PACK OF 3 (FREE SIZE)",
+    src:
+      "https://www.uboric.com/wp-content/uploads/2020/12/WCTA03-059_1024x1024.png",
+  },
+  1: {
+    company_name: "Luxury Terry",
+    tittle: "ASHIKAGA EDITION SOCKS",
+    price: "299.00",
+    description: "Men’s Luxury Terry Cushioning and Spandex Solid Ankle Socks",
+    src:
+      "https://www.uboric.com/wp-content/uploads/2020/11/socksoho-comfortable-fun-men-socks-stripes-ashikaga-edition-13861452611635_600x.png",
+  },
+  2: {
+    company_name: "Luxury Terry",
+    tittle: "BLUEBERRY EDITION SOCKS",
+    price: "299.00",
+    description:
+      "Men’s Luxury Terry Cushioning and Spandex BLUEBERRY EDITION SOCKS",
+    src:
+      "https://www.uboric.com/wp-content/uploads/2020/11/Blueberry_2_600x.png",
+  },
+  3: {
+    company_name: "CLASSIC BLACK",
+    tittle: "THE GENTLEMAN EDITION SOCKS",
+    price: "399.00",
+    description:
+      "Men’s Luxury Terry Cushioning and Spandex LONDON EDITION SOCKS",
+    src: "https://www.uboric.com/wp-content/uploads/2020/11/Gentleman_600x.png",
+  },
+  4: {
+    company_name: "CLASSIC BLACK",
+    tittle: "CLASSIC BLACK EDITION SOCKS",
+    price: "349.00",
+    description:
+      "Men’s Luxury Terry Cushioning and Spandex CLASSIC BLACK EDITION SOCKS",
+    src:
+      "https://www.uboric.com/wp-content/uploads/2020/11/socksoho-comfortable-men-socks-black-classic-black-edition-13861452644403_600x.png",
+  },
+  5: {
+    company_name: "CLASSIC BLACK",
+    tittle: "HAPPY SOCKS GIFT BOX FOR MEN",
+    price: "1,599.00",
+    description:
+      "Men’s Luxury Terry Cushioning and Spandex CLASSIC BLACK EDITION SOCKS",
+    src:
+      "https://www.uboric.com/wp-content/uploads/2020/11/POWERBOX_c4c4b190-aba2-4629-8bfe-8e7d0507d99b_600x.png",
+  },
+  6: {
+    company_name: "CLASSIC BLACK",
+    tittle: "LONDON EDITION SOCKS",
+    price: "349.00",
+    description:
+      "Men’s Luxury Terry Cushioning and Spandex LONDON EDITION SOCKS",
+    src:
+      "https://www.uboric.com/wp-content/uploads/2020/11/socksoho-cute-quirky-men-socks-london-edition-13862745604147_600x.png",
+  },
+  7: {
+    company_name: "CLASSIC BLACK",
+    tittle: "THE GENTLEMAN EDITION SOCKS",
+    price: "399.00",
+    description:
+      "Men’s Luxury Terry Cushioning and Spandex LONDON EDITION SOCKS",
+    src: "https://www.uboric.com/wp-content/uploads/2020/11/Gentleman_600x.png",
+  },
+  8: {
+    company_name: "Luxury Terry",
+    tittle: "ASHIKAGA EDITION SOCKS",
+    price: "299.00",
+    description: "Men’s Luxury Terry Cushioning and Spandex Solid Ankle Socks",
+    src:
+      "https://www.uboric.com/wp-content/uploads/2020/11/socksoho-comfortable-fun-men-socks-stripes-ashikaga-edition-13861452611635_600x.png",
+  },
+  9: {
+    company_name: "CLASSIC BLACK",
+    tittle: "THE GENTLEMAN EDITION SOCKS",
+    price: "399.00",
+    description:
+      "Men’s Luxury Terry Cushioning and Spandex LONDON EDITION SOCKS",
+    src: "https://www.uboric.com/wp-content/uploads/2020/11/Gentleman_600x.png",
+  },
+  10: {
+    company_name: "CLASSIC BLACK",
+    tittle: "LONDON EDITION SOCKS",
+    price: "349.00",
+    description:
+      "Men’s Luxury Terry Cushioning and Spandex LONDON EDITION SOCKS",
+    src:
+      "https://www.uboric.com/wp-content/uploads/2020/11/socksoho-cute-quirky-men-socks-london-edition-13862745604147_600x.png",
+  },
+  11: {
+    Company_name: "CLASSIC BLACK",
+    tittle: "HAPPY SOCKS GIFT BOX FOR MEN",
+    price: "1,599.00",
+    description:
+      "Men’s Luxury Terry Cushioning and Spandex CLASSIC BLACK EDITION SOCKS",
+    src:
+      "https://www.uboric.com/wp-content/uploads/2020/11/POWERBOX_c4c4b190-aba2-4629-8bfe-8e7d0507d99b_600x.png",
+  },
+  12: {
+    Company_name: "CLASSIC BLACK",
+    tittle: "CLASSIC BLACK EDITION SOCKS",
+    price: "349.00",
+    description:
+      "Men’s Luxury Terry Cushioning and Spandex CLASSIC BLACK EDITION SOCKS",
+    src:
+      "https://www.uboric.com/wp-content/uploads/2020/11/socksoho-comfortable-men-socks-black-classic-black-edition-13861452644403_600x.png",
+  },
+  13: {
+    Company_name: "Luxury Terry",
+    tittle: "BLUEBERRY EDITION SOCKS",
+    price: "299.00",
+    description:
+      "Men’s Luxury Terry Cushioning and Spandex BLUEBERRY EDITION SOCKS",
+    src:
+      "https://www.uboric.com/wp-content/uploads/2020/11/Blueberry_2_600x.png",
+  },
+  14: {
+    Company_name: "Luxury Terry",
+    tittle: "ASHIKAGA EDITION SOCKS",
+    price: "299.00",
+    description: "Men’s Luxury Terry Cushioning and Spandex Solid Ankle Socks",
+    src:
+      "https://www.uboric.com/wp-content/uploads/2020/11/socksoho-comfortable-fun-men-socks-stripes-ashikaga-edition-13861452611635_600x.png",
+  },
+};
+
+export default data;
