a5bef8d5976a4c12e69a3b7c99a6ebf414e0bd1b
k.saikrishna <k.saikrishna78@gmail.com>
2021-03-12 13:00:19 +0530

    the correction in display of products

--- src/js/utils/createGrocery.js
+++ src/js/utils/createGrocery.js
@@ -1,11 +1,11 @@
-function createGrocery(data) {
+function createGrocery(data,key) {
   const productStr = `
-  <article class="product display-flex">
-    <img src=${data.src} alt=${data.tittle} class="item-img"
+  <article data-id="${key}" class="product display-flex">
+    <img data-id="${key}" src=${data.src} alt=${data.tittle} class="item-img"
     />
-    <h3><span class=${data.tittle[0]=="A"}>${data.tittle}</span></h3>
+    <h3 data-id="${key}"><span data-id="${key}" class=${data.tittle[0]=="A"}>${data.tittle}</span></h3>
     <p class="price">
-      <span class="new-price"> ₹${data.price} </span>
+      <span data-id="${key}" class="new-price"> ₹${data.price} </span>
     </p>
   </article>
   `;
